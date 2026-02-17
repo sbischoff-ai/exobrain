c34add6348866c8cd9e7d0cf6c1dfa7a25f0e050
Silas Bischoff <contact@sbischoff.dev>
2026-02-17 17:05:07 +0100

    fix(infra): create assistant db outside plpgsql blocks

--- infra/docker/metastore/init/01-assistant-db.sql
+++ infra/docker/metastore/init/01-assistant-db.sql
@@ -1,18 +1,12 @@
-DO $$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'assistant_backend') THEN
-      CREATE ROLE assistant_backend LOGIN PASSWORD 'assistant_backend';
-   END IF;
-END
-$$;
+SELECT 'CREATE ROLE assistant_backend LOGIN PASSWORD ''assistant_backend''' 
+WHERE NOT EXISTS (
+  SELECT FROM pg_catalog.pg_roles WHERE rolname = 'assistant_backend'
+)\gexec
 
-DO $$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'assistant_db') THEN
-      CREATE DATABASE assistant_db OWNER assistant_backend;
-   END IF;
-END
-$$;
+SELECT 'CREATE DATABASE assistant_db OWNER assistant_backend'
+WHERE NOT EXISTS (
+  SELECT FROM pg_database WHERE datname = 'assistant_db'
+)\gexec
 
 REVOKE ALL ON DATABASE assistant_db FROM PUBLIC;
 GRANT CONNECT, TEMPORARY ON DATABASE assistant_db TO assistant_backend;
